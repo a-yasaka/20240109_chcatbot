0aab7cf41a262871329b6046d4e2ac5d66452fde
a-yasaka <aki.ysk@gmail.com>
2024-01-09 09:57:38 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def communicate():
         messages=messages
     )
 
-    bot_message = {"roll": response.choices[0].message.roll, "content": response.choices[0].message.content}
+    bot_message = {"role": response.choices[0].message.role, "content": response.choices[0].message.content}
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
